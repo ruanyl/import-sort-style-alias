bed21d01b1f93d39915660dc0e29f71d791410df
Yulong Ruan <yruan@alpha-sense.com>
2018-10-19 12:29:10 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,17 @@
-import * as findRoot from "find-root";
 import {extname} from "path";
 import {readFileSync} from "fs";
 import {IImport} from "import-sort-parser";
 import {IStyleAPI, IStyleItem} from "import-sort-style";
 
-const isAlias = (alias: string) => (imported: IImport) => imported.moduleName.indexOf(alias) === 0;
+const isAlias = (alias: string[]) => (moduleName: string) => alias.indexOf(moduleName.split("/")[0]) >= 0;
 
 const hasAlias = (alias: any) => (imported: IImport) => {
   return alias.some((a: string) => imported.moduleName.indexOf(a) === 0);
 };
 
-const createAliasRules = (styleApi: IStyleAPI, alias: string[]): IStyleItem[] => {
-  const {
-    and,
-    hasDefaultMember,
-    hasNamedMembers,
-    hasNamespaceMember,
-    hasNoMember,
-    hasOnlyDefaultMember,
-    hasOnlyNamedMembers,
-    hasOnlyNamespaceMember,
-    isAbsoluteModule,
-    isRelativeModule,
-    member,
-    name,
-    not,
-    startsWithAlphanumeric,
-    startsWithLowerCase,
-    startsWithUpperCase,
-    unicode,
-  } = styleApi;
-
-  return alias.reduce((accumulator: IStyleItem[], currentValue: string) => {
-    return accumulator.concat([
-      // import * as _ from "alias/name";
-      {match: and(hasOnlyNamespaceMember, isAbsoluteModule, isAlias(currentValue), not(member(startsWithAlphanumeric))), sort: member(unicode)},
-      // import * as Foo from "bar";
-      {match: and(hasOnlyNamespaceMember, isAbsoluteModule, isAlias(currentValue), member(startsWithUpperCase)), sort: member(unicode)},
-      // import * as foo from "bar";
-      {match: and(hasOnlyNamespaceMember, isAbsoluteModule, isAlias(currentValue), member(startsWithLowerCase)), sort: member(unicode)},
-
-      // import _, * as bar from "baz";
-      {match: and(hasDefaultMember, hasNamespaceMember, isAbsoluteModule, isAlias(currentValue), not(member(startsWithAlphanumeric))), sort: member(unicode)},
-      // import Foo, * as bar from "baz";
-      {match: and(hasDefaultMember, hasNamespaceMember, isAbsoluteModule, isAlias(currentValue), member(startsWithUpperCase)), sort: member(unicode)},
-      // import foo, * as bar from "baz";
-      {match: and(hasDefaultMember, hasNamespaceMember, isAbsoluteModule, isAlias(currentValue), member(startsWithUpperCase)), sort: member(unicode)},
-
-      // import _ from "bar";
-      {match: and(hasOnlyDefaultMember, isAbsoluteModule, isAlias(currentValue), not(member(startsWithAlphanumeric))), sort: member(unicode)},
-      // import Foo from "bar";
-      {match: and(hasOnlyDefaultMember, isAbsoluteModule, isAlias(currentValue), member(startsWithUpperCase)), sort: member(unicode)},
-      // import foo from "bar";
-      {match: and(hasOnlyDefaultMember, isAbsoluteModule, isAlias(currentValue), member(startsWithLowerCase)), sort: member(unicode)},
-
-      // import _, {bar, …} from "baz";
-      {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, isAlias(currentValue), not(member(startsWithAlphanumeric))), sort: member(unicode), sortNamedMembers: name(unicode)},
-      // import Foo, {bar, …} from "baz";
-      {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, isAlias(currentValue), member(startsWithUpperCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
-      // import foo, {bar, …} from "baz";
-      {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, isAlias(currentValue), member(startsWithLowerCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
-
-      // import {_, bar, …} from "baz";
-      {match: and(hasOnlyNamedMembers, isAbsoluteModule, isAlias(currentValue), not(member(startsWithAlphanumeric))), sort: member(unicode), sortNamedMembers: name(unicode)},
-      // import {Foo, bar, …} from "baz";
-      {match: and(hasOnlyNamedMembers, isAbsoluteModule, isAlias(currentValue), member(startsWithUpperCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
-      // import {foo, bar, …} from "baz";
-      {match: and(hasOnlyNamedMembers, isAbsoluteModule, isAlias(currentValue), member(startsWithLowerCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
-    ]);
-  }, []);
-};
+const customSort = (comparator) =>
+  (first: string, second: string) =>
+  comparator(first.toLowerCase(), second.toLowerCase());
 
 export default function(styleApi: IStyleAPI, file?: string, options?: any): IStyleItem[] {
   const {
@@ -90,10 +32,25 @@ export default function(styleApi: IStyleAPI, file?: string, options?: any): ISty
     startsWithLowerCase,
     startsWithUpperCase,
     unicode,
+    moduleName,
   } = styleApi;
 
   const alias = options.alias || [];
-  const customRules: IStyleItem[] = createAliasRules(styleApi, alias);
+
+  let sortBy;
+  switch (options.sortBy) {
+    case "moduleName":
+      sortBy = moduleName;
+      break;
+    case "member":
+      sortBy = member;
+      break;
+    default:
+      sortBy = moduleName;
+      break;
+  }
+
+  const comparator = options.ignoreCase === false ? unicode : customSort(unicode);
 
   return [
     // import "foo"
@@ -107,17 +64,13 @@ export default function(styleApi: IStyleAPI, file?: string, options?: any): ISty
     // import * as _ from "bar";
     {
       match: and(hasOnlyNamespaceMember, isAbsoluteModule, not(hasAlias(alias)), not(member(startsWithAlphanumeric))),
-      sort: member(unicode),
+      sort: sortBy(comparator),
     },
     // import * as Foo from "bar";
-    {
-      match: and(hasOnlyNamespaceMember, isAbsoluteModule, not(hasAlias(alias)), member(startsWithUpperCase)),
-      sort: member(unicode),
-    },
     // import * as foo from "bar";
     {
-      match: and(hasOnlyNamespaceMember, isAbsoluteModule, not(hasAlias(alias)), member(startsWithLowerCase)),
-      sort: member(unicode),
+      match: and(hasOnlyNamespaceMember, isAbsoluteModule, not(hasAlias(alias))),
+      sort: sortBy(comparator),
     },
 
     // import _, * as bar from "baz";
@@ -129,74 +82,93 @@ export default function(styleApi: IStyleAPI, file?: string, options?: any): ISty
         not(hasAlias(alias)),
         not(member(startsWithAlphanumeric)),
       ),
-      sort: member(unicode),
+      sort: sortBy(comparator),
     },
     // import Foo, * as bar from "baz";
-    {match: and(hasDefaultMember, hasNamespaceMember, isAbsoluteModule, not(hasAlias(alias)), member(startsWithUpperCase)), sort: member(unicode)},
     // import foo, * as bar from "baz";
-    {match: and(hasDefaultMember, hasNamespaceMember, isAbsoluteModule, not(hasAlias(alias)), member(startsWithUpperCase)), sort: member(unicode)},
+    {match: and(hasDefaultMember, hasNamespaceMember, isAbsoluteModule, not(hasAlias(alias))), sort: sortBy(comparator)},
 
     // import _ from "bar";
-    {match: and(hasOnlyDefaultMember, isAbsoluteModule, not(hasAlias(alias)), not(member(startsWithAlphanumeric))), sort: member(unicode)},
+    {match: and(hasOnlyDefaultMember, isAbsoluteModule, not(hasAlias(alias)), not(member(startsWithAlphanumeric))), sort: sortBy(comparator)},
     // import Foo from "bar";
-    {match: and(hasOnlyDefaultMember, isAbsoluteModule, not(hasAlias(alias)), member(startsWithUpperCase)), sort: member(unicode)},
     // import foo from "bar";
-    {match: and(hasOnlyDefaultMember, isAbsoluteModule, not(hasAlias(alias)), member(startsWithLowerCase)), sort: member(unicode)},
+    {match: and(hasOnlyDefaultMember, isAbsoluteModule, not(hasAlias(alias))), sort: sortBy(comparator)},
 
     // import _, {bar, …} from "baz";
-    {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, not(hasAlias(alias)), not(member(startsWithAlphanumeric))), sort: member(unicode), sortNamedMembers: name(unicode)},
+    {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, not(hasAlias(alias)), not(member(startsWithAlphanumeric))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
     // import Foo, {bar, …} from "baz";
-    {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, not(hasAlias(alias)), member(startsWithUpperCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
     // import foo, {bar, …} from "baz";
-    {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, not(hasAlias(alias)), member(startsWithLowerCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
+    {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, not(hasAlias(alias))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
 
     // import {_, bar, …} from "baz";
-    {match: and(hasOnlyNamedMembers, isAbsoluteModule, not(hasAlias(alias)), not(member(startsWithAlphanumeric))), sort: member(unicode), sortNamedMembers: name(unicode)},
+    {match: and(hasOnlyNamedMembers, isAbsoluteModule, not(hasAlias(alias)), not(member(startsWithAlphanumeric))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
     // import {Foo, bar, …} from "baz";
-    {match: and(hasOnlyNamedMembers, isAbsoluteModule, not(hasAlias(alias)), member(startsWithUpperCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
     // import {foo, bar, …} from "baz";
-    {match: and(hasOnlyNamedMembers, isAbsoluteModule, not(hasAlias(alias)), member(startsWithLowerCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
-
-    {separator: true},
-
-    ...customRules,
+    {match: and(hasOnlyNamedMembers, isAbsoluteModule, not(hasAlias(alias))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
 
     {separator: true},
 
     // import * as _ from "./bar";
-    {match: and(hasOnlyNamespaceMember, isRelativeModule, not(member(startsWithAlphanumeric))), sort: member(unicode)},
+    {match: and(hasOnlyNamespaceMember, isRelativeModule, not(member(startsWithAlphanumeric))), sort: sortBy(comparator)},
     // import * as Foo from "./bar";
-    {match: and(hasOnlyNamespaceMember, isRelativeModule, member(startsWithUpperCase)), sort: member(unicode)},
     // import * as foo from "./bar";
-    {match: and(hasOnlyNamespaceMember, isRelativeModule, member(startsWithLowerCase)), sort: member(unicode)},
+    {match: and(hasOnlyNamespaceMember, isRelativeModule), sort: sortBy(comparator)},
 
     // import _, * as bar from "./baz";
-    {match: and(hasDefaultMember, hasNamespaceMember, isRelativeModule, not(member(startsWithAlphanumeric))), sort: member(unicode)},
+    {match: and(hasDefaultMember, hasNamespaceMember, isRelativeModule, not(member(startsWithAlphanumeric))), sort: sortBy(comparator)},
     // import Foo, * as bar from "./baz";
-    {match: and(hasDefaultMember, hasNamespaceMember, isRelativeModule, member(startsWithUpperCase)), sort: member(unicode)},
     // import foo, * as bar from "./baz";
-    {match: and(hasDefaultMember, hasNamespaceMember, isRelativeModule, member(startsWithUpperCase)), sort: member(unicode)},
+    {match: and(hasDefaultMember, hasNamespaceMember, isRelativeModule), sort: sortBy(comparator)},
 
     // import _ from "./bar";
-    {match: and(hasOnlyDefaultMember, isRelativeModule, not(member(startsWithAlphanumeric))), sort: member(unicode)},
+    {match: and(hasOnlyDefaultMember, isRelativeModule, not(member(startsWithAlphanumeric))), sort: sortBy(comparator)},
     // import Foo from "./bar";
-    {match: and(hasOnlyDefaultMember, isRelativeModule, member(startsWithUpperCase)), sort: member(unicode)},
     // import foo from "./bar";
-    {match: and(hasOnlyDefaultMember, isRelativeModule, member(startsWithLowerCase)), sort: member(unicode)},
+    {match: and(hasOnlyDefaultMember, isRelativeModule), sort: sortBy(comparator)},
 
     // import _, {bar, …} from "./baz";
-    {match: and(hasDefaultMember, hasNamedMembers, isRelativeModule, not(member(startsWithAlphanumeric))), sort: member(unicode), sortNamedMembers: name(unicode)},
+    {match: and(hasDefaultMember, hasNamedMembers, isRelativeModule, not(member(startsWithAlphanumeric))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
     // import Foo, {bar, …} from "./baz";
-    {match: and(hasDefaultMember, hasNamedMembers, isRelativeModule, member(startsWithUpperCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
     // import foo, {bar, …} from "./baz";
-    {match: and(hasDefaultMember, hasNamedMembers, isRelativeModule, member(startsWithLowerCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
+    {match: and(hasDefaultMember, hasNamedMembers, isRelativeModule), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
 
     // import {_, bar, …} from "./baz";
-    {match: and(hasOnlyNamedMembers, isRelativeModule, not(member(startsWithAlphanumeric))), sort: member(unicode), sortNamedMembers: name(unicode)},
+    {match: and(hasOnlyNamedMembers, isRelativeModule, not(member(startsWithAlphanumeric))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
     // import {Foo, bar, …} from "./baz";
-    {match: and(hasOnlyNamedMembers, isRelativeModule, member(startsWithUpperCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
     // import {foo, bar, …} from "./baz";
-    {match: and(hasOnlyNamedMembers, isRelativeModule, member(startsWithLowerCase)), sort: member(unicode), sortNamedMembers: name(unicode)},
+    {match: and(hasOnlyNamedMembers, isRelativeModule), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
+
+    {separator: true},
+
+    // import * as _ from "alias/name";
+    {match: and(hasOnlyNamespaceMember, isAbsoluteModule, moduleName(isAlias(alias)), not(member(startsWithAlphanumeric))), sort: sortBy(comparator)},
+    // import * as Foo from "bar";
+    // import * as foo from "bar";
+    {match: and(hasOnlyNamespaceMember, isAbsoluteModule, moduleName(isAlias(alias))), sort: sortBy(comparator)},
+
+    // import _, * as bar from "baz";
+    {match: and(hasDefaultMember, hasNamespaceMember, isAbsoluteModule, moduleName(isAlias(alias)), not(member(startsWithAlphanumeric))), sort: sortBy(comparator)},
+    // import Foo, * as bar from "baz";
+    // import foo, * as bar from "baz";
+    {match: and(hasDefaultMember, hasNamespaceMember, isAbsoluteModule, moduleName(isAlias(alias))), sort: sortBy(comparator)},
+
+    // import _ from "bar";
+    {match: and(hasOnlyDefaultMember, isAbsoluteModule, moduleName(isAlias(alias)), not(member(startsWithAlphanumeric))), sort: sortBy(comparator)},
+    // import Foo from "bar";
+    // import foo from "bar";
+    {match: and(hasOnlyDefaultMember, isAbsoluteModule, moduleName(isAlias(alias))), sort: sortBy(comparator)},
+
+    // import _, {bar, …} from "baz";
+    {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, moduleName(isAlias(alias)), not(member(startsWithAlphanumeric))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
+    // import Foo, {bar, …} from "baz";
+    // import foo, {bar, …} from "baz";
+    {match: and(hasDefaultMember, hasNamedMembers, isAbsoluteModule, moduleName(isAlias(alias))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
+
+    // import {_, bar, …} from "baz";
+    {match: and(hasOnlyNamedMembers, isAbsoluteModule, moduleName(isAlias(alias)), not(member(startsWithAlphanumeric))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
+    // import {Foo, bar, …} from "baz";
+    // import {foo, bar, …} from "baz";
+    {match: and(hasOnlyNamedMembers, isAbsoluteModule, moduleName(isAlias(alias))), sort: sortBy(comparator), sortNamedMembers: name(comparator)},
 
     {separator: true},
   ];
